3d9fd0952454d2bfb9e4f361bcc28551bf935d82
KYOYA OGASAWARA <kyoyaoga@gmail.com>
2022-12-09 13:25:30 +0700

    created home page

--- components/BlockRenderer/BlockRenderer.js
+++ components/BlockRenderer/BlockRenderer.js
@@ -1,16 +1,31 @@
+import CallToActionButton from "components/CallToActionButton/CallToActionButton";
+import Column from "components/Column/Column";
+import Columns from "components/Columns/Columns";
 import Cover from "components/Cover/Cover";
 import Heading from "components/Heading/Heading";
 import Paragraph from "components/Paragraph/Paragraph";
+import Image from "next/image";
 import { theme } from "theme";
 
 const BlockRenderer = ({ blocks }) => {
   return blocks.map((block) => {
     switch (block.name) {
+      case "acf/ctabutton": {
+        return (
+          <CallToActionButton
+            key={block.id}
+            buttonLabel={block.attributes.data.label}
+            destination={block.attributes.data.destination?.slug || "/"}
+            align={block.attributes.data.align}
+          />
+        );
+      }
       case "core/paragraph": {
         return (
           <Paragraph
             key={block.id}
             content={block.attributes.content}
+            fontSize={block.attributes.style?.typography.fontSize}
             textAlign={block.attributes.align}
             textColor={
               theme[block.attributes.textColor] ||
@@ -37,7 +52,44 @@ const BlockRenderer = ({ blocks }) => {
         );
       }
 
+      case "core/columns": {
+        return (
+          <Columns
+            key={block.id}
+            isStackedOnMobile={block.attributes.isStackedOnMobile}
+          >
+            <BlockRenderer blocks={block.innerBlocks} />
+          </Columns>
+        );
+      }
+
+      case "core/column": {
+        return (
+          <Column key={block.id} width={block.attributes.width}>
+            <BlockRenderer blocks={block.innerBlocks} />
+          </Column>
+        );
+      }
+
+      case "core/block":
+      case "core/group": {
+        return <BlockRenderer key={block.id} blocks={block.innerBlocks} />;
+      }
+
+      case "core/image": {
+        return (
+          <Image
+            key={block.id}
+            src={block.attributes.url}
+            width={block.attributes.width}
+            height={block.attributes.height}
+            alt={block.attributes.alt}
+          />
+        );
+      }
+
       default: {
+        // console.log("BlockRenderer: Unknown block type", block);
         return null;
       }
     }
--- components/Paragraph/Paragraph.js
+++ components/Paragraph/Paragraph.js
@@ -1,11 +1,16 @@
 import { getTextAlign } from "utils/fonts";
 import { relativeToAbsoluteUrls } from "utils/relativeToAbsoluteUrls";
 
-function Paragraph({ textAlign = "left", content, textColor }) {
+function Paragraph({
+  textAlign = "left",
+  content,
+  textColor,
+  fontSize = "1rem",
+}) {
   return (
     <p
       className={`max-w-5xl mx-auto ${getTextAlign(textAlign)}`}
-      style={{ color: textColor }}
+      style={{ color: textColor, fontSize: fontSize }}
       dangerouslySetInnerHTML={{ __html: relativeToAbsoluteUrls(content) }}
     />
   );
